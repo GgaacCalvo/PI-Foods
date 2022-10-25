9ea509ad36b6472fe4dbdbd3a47fb1b2322c7acf
Guillermo Calvo <guillecalvo5@gmail.com>
2022-10-24 22:36:59 -0500

    before deploy

--- client/src/componentes/Home.jsx
+++ client/src/componentes/Home.jsx
@@ -1,138 +1,135 @@
 import { Link } from "react-router-dom";
-import {useDispatch, useSelector} from 'react-redux'
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
-import { getDiets, getRecipes, orderByName, orderByScore, filterRecipesByDiet } from "../store/actions";
-import {Card} from './Card'
-import {Search} from './Search'
+import {
+  getDiets,
+  getRecipes,
+  orderByName,
+  orderByScore,
+  filterRecipesByDiet,
+} from "../store/actions";
+import { Card } from "./Card";
+import { Search } from "./Search";
 import { Paginado } from "./Paginado";
 import { useState } from "react";
-import "./Home.css"
+import "./Home.css";
 import { NavBar } from "./NavBar";
 import { Loading } from "./Loading";
+import * as React from "react";
+import Button from "@mui/material/Button";
 
+export function Home() {
+  let recetas = useSelector((state) => state.recetas); // esto es para traer el state, se puede poner como parametro de la funcion y acceder desde match.state.props (creo que hay que importar esto, es para funciones de clases)
 
-export function Home(){
-     let recetas = useSelector((state) => state.recetas) // esto es para traer el state, se puede poner como parametro de la funcion y acceder desde match.state.props (creo que hay que importar esto, es para funciones de clases)
-     
-     const dietasTotales = useSelector((state) => state.dietas) // aca me traigo todas las dietas del state
-     
-     //traerme el dispatch con el hook useDistpatch
-     let dispatch = useDispatch();
-     
-    const [currentPage, setCurrentPage] = useState(1);
-    const [recipesPerPage, setRecipesPerPage] = useState(9);
-    const endOfthePage = currentPage * recipesPerPage;
-    const indexOfFirstRecipePage = endOfthePage - recipesPerPage;
-    const currentRecipes = recetas.slice(indexOfFirstRecipePage, endOfthePage) ;   
+  const dietasTotales = useSelector((state) => state.dietas); // aca me traigo todas las dietas del state
 
+  //traerme el dispatch con el hook useDistpatch
+  let dispatch = useDispatch();
 
-    const paginado = (pageNumber) => {
-        setCurrentPage(pageNumber)
-    }
+  const [currentPage, setCurrentPage] = useState(1);
+  const [recipesPerPage, setRecipesPerPage] = useState(9);
+  const endOfthePage = currentPage * recipesPerPage;
+  const indexOfFirstRecipePage = endOfthePage - recipesPerPage;
+  const currentRecipes = recetas.slice(indexOfFirstRecipePage, endOfthePage);
 
-    
-    useEffect(() => {
-        dispatch(getRecipes())
-        dispatch(getDiets())
-        }, [dispatch])
-        
-        
+  const paginado = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
+  useEffect(() => {
+    dispatch(getRecipes());
+    dispatch(getDiets());
+  }, [dispatch]);
 
-    function handleOrderName(e) {
-        e.preventDefault()
-        dispatch(orderByName(e.target.value))
-    }
-    function handleOrderScore(e) {
-        e.preventDefault()
-        dispatch(orderByScore(e.target.value))
-    }
-    function handleDiets(e) {
-        e.preventDefault()
-        dispatch(filterRecipesByDiet(e.target.value));
-        setCurrentPage(1)
-    }
-    
-    function handleReset(e) {
-        e.preventDefault()
-        window.location.reload(); //refresh (f5)
-        
-    }
-    return (
-        
-        <div className="contenedor-home">
-            <div className="contenedor-pag">
+  function handleOrderName(e) {
+    e.preventDefault();
+    dispatch(orderByName(e.target.value));
+  }
+  function handleOrderScore(e) {
+    e.preventDefault();
+    dispatch(orderByScore(e.target.value));
+  }
+  function handleDiets(e) {
+    e.preventDefault();
+    dispatch(filterRecipesByDiet(e.target.value));
+    setCurrentPage(1);
+  }
 
-            <div className="Nav-Bar-home">
-                <NavBar/>
+  function handleReset(e) {
+    e.preventDefault();
+    window.location.reload(); //refresh (f5)
+  }
+  return (
+    <div className="contenedor-home">
+      <div className="contenedor-pag">
+        <div className="Nav-Bar-home">
+          <NavBar />
+        </div>
+        <div className="cont-except-nav">
+          <Link to="/createrecipe">
+            <div className="createrecipe-home">
+              <Button variant="contained" color="success">
+                CREATE RECIPE
+              </Button>
+              {/* <h3 className="createrecipe-home">CREATE RECIPE</h3> */}
             </div>
-            <div className="cont-except-nav">               
-                <Link to="/createrecipe">
-                    <div className="createrecipe-home">
-
-                       <h3 className="createrecipe-home">CREATE RECIPE</h3>
-                        </div>
-                </Link>
-                <Search/>
-                <select className="select-home" onChange={handleOrderName}>
-                    <option value='all'>All</option>
-                    <option value='asc'>A to Z</option>
-                    <option value='desc'>Z to A</option>
-                </select>
-                <select className="select-home" onChange={handleOrderScore}>
-                    <option value='all'>All</option>
-                    <option value='low'>Low Score</option>
-                    <option value='high'>High Score</option>
-                </select>
-                <select className="select-home" onChange={handleDiets}>
-                    <option value='All'>All</option>
-                    {console.log(dietasTotales)}
-                    {dietasTotales?.map((d) => (
-                        <option key={d.id} value={d.name}> {d.name} </option>
-                    ))}
-                </select>
-                <button className="button-reset-home" onClick={handleReset}>RESET</button>
-
-            </div>
-            {
-                currentRecipes.length === 0 && currentRecipes ? (
-                    <Loading/>):(
-                        <div>
-
-                    <div className="cards">
-
-                {
-                    currentRecipes.map((receta) => {
-                        
-                        return (
-                            <Link key={`${receta.id}`} to={`/recipes/${receta.id}`}>
-                                <Card 
-                                key={receta.id} 
-                                id={receta.id}
-                                name={receta.name}                      
-                                image={receta.image}
-                                diets={receta.diets}
-                                healthScore={receta.healthScore}
-                                />
-                            </Link>
-                        )
-                    })
-                } 
+          </Link>
+          <h1 className="msjpre">Do you know what to cook today?</h1>
+          <Search />
+          <select className="select-home" onChange={handleOrderName}>
+            <option value="all">All</option>
+            <option value="asc">A to Z</option>
+            <option value="desc">Z to A</option>
+          </select>
+          <select className="select-home" onChange={handleOrderScore}>
+            <option value="all">All</option>
+            <option value="low">Low Score</option>
+            <option value="high">High Score</option>
+          </select>
+          <select className="select-home" onChange={handleDiets}>
+            <option value="All">All</option>
+            {console.log(dietasTotales)}
+            {dietasTotales?.map((d) => (
+              <option key={d.id} value={d.name}>
+                {" "}
+                {d.name}{" "}
+              </option>
+            ))}
+          </select>
+          <button className="button-reset-home" onClick={handleReset}>
+            RESET
+          </button>
+        </div>
+        {currentRecipes.length === 0 && currentRecipes ? (
+          <Loading />
+        ) : (
+          <div>
+            <div className="cards">
+              {currentRecipes.map((receta) => {
+                return (
+                  <Link key={`${receta.id}`} to={`/recipes/${receta.id}`}>
+                    <Card
+                      key={receta.id}
+                      id={receta.id}
+                      name={receta.name}
+                      image={receta.image}
+                      diets={receta.diets}
+                      healthScore={receta.healthScore}
+                    />
+                  </Link>
+                );
+              })}
             </div>
             <div className="paginado-home">
-
-            <Paginado
+              <Paginado
                 recipesPerPage={recipesPerPage}
                 recetas={recetas.length}
                 paginado={paginado}
-                />
+              />
             </div>
-            </div>
-                )
-            }
-        </div>
-        </div>
-        
-        )
-        
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
--- client/src/componentes/Search.jsx
+++ client/src/componentes/Search.jsx
@@ -1,41 +1,47 @@
-import {useState} from 'react'
-import "./Search.css"
-import { useDispatch } from 'react-redux';
-import { getRecipesName } from '../store/actions';
-import { useSelector } from 'react-redux';
+import { useState } from "react";
+import "./Search.css";
+import { useDispatch } from "react-redux";
+import { getRecipesName } from "../store/actions";
+import { useSelector } from "react-redux";
 
-export  function Search() {
-    const [search, setSearch] = useState('');
-    let dispatch = useDispatch();
+export function Search() {
+  const [search, setSearch] = useState("");
+  let dispatch = useDispatch();
 
+  const recipes = useSelector((state) => state.recetas);
 
-const recipes = useSelector((state) => state.recetas)
-
-    const onSubmit =  (e) => {
-        e.preventDefault()
-        validate()
-        if(!search.trim()){
-            return alert("Please insert a valid food name")
-        } else {
-            dispatch( getRecipesName(search.trim()))  
-            console.log(recipes)           
-            setSearch('')     
-        }
+  const onSubmit = (e) => {
+    e.preventDefault();
+    validate();
+    if (!search.trim()) {
+      return alert("Please insert a valid food name");
+    } else {
+      dispatch(getRecipesName(search.trim()));
+      console.log(recipes);
+      setSearch("");
     }
-    const validate = () => {
-        if(!search.trim()){
-            return alert("Please insert a valid food name")
-        }         
+  };
+  const validate = () => {
+    if (!search.trim()) {
+      return alert("Please insert a valid food name");
     }
-function onInputChange(e) {
-    setSearch(e.target.value)    
-}
+  };
+  function onInputChange(e) {
+    setSearch(e.target.value);
+  }
 
-    return <div>
-        
-            <input className='input-search' type="text" placeholder="Search your recipe" onChange={onInputChange} value={search}/>
-            <button className="btnSearch" type="submit" onClick={onSubmit}>SEARCH</button>
-            
+  return (
+    <div className="cont-search">
+      <input
+        className="input-search"
+        type="text"
+        placeholder="Search your recipe"
+        onChange={onInputChange}
+        value={search}
+      />
+      <button className="btnSearch" type="submit" onClick={onSubmit}>
+        SEARCH
+      </button>
     </div>
+  );
 }
-
