271375fc0c859998527f9c9f24ecccb7fb614429
Guillermo Calvo <guillecalvo5@gmail.com>
2022-10-27 23:51:24 -0500

    last prueba

--- api/src/app.js
+++ api/src/app.js
@@ -24,7 +24,7 @@ server.use(morgan('dev'));
    next();
  });
 
-server.use('/api', routes);
+server.use('/', routes);
 
 // Error catching endware.
 server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
