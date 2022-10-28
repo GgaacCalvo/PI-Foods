c9293fc1223d4065b8e432f7c120381430d1c159
Guillermo Calvo <guillecalvo5@gmail.com>
2022-10-27 23:45:34 -0500

    nuevaprueba

--- api/index.js
+++ api/index.js
@@ -22,14 +22,19 @@ const { conn } = require('./src/db.js');
 
 // ASD
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
-  server.listen(process.env.PORT, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
-  });
-});
+// conn.sync({ force: false }).then(() => {
+//   server.listen(process.env.PORT, () => {
+//     console.log('%s listening at 3001'); // eslint-disable-line no-console
+//   });
+// });
 /*
 .then(() => {
   dietTypes.map(e => DietType.create({ name: e }));
 });
 const dietTypes = ['gluten free', 'ketogenic', 'vegetarian', 'lacto vegetarian','ovo vegetarian', 'lacto ovo vegetarian', 'vegan', 'pescetarian', 'paleolithic', 'primal', 'low fodmap', 'whole 30', 'dairy free'];
 */
+conn.sync({ force: true }).then(() => {
+  server.listen(process.env.PORT, () => {
+    console.log("%s listening at 3000"); // eslint-disable-line no-console
+  });
+});
